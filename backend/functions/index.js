cb32624ea87af70eca7c8e0f265cfb86267582e0
Joanne <joannechen84@gmail.com>
2021-07-17 15:52:18 +1000

    removed backend folder, added template function for twilio

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require("firebase-functions");
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,12 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
+
