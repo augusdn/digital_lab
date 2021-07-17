c1bcd25b6f69e1aaef66127e60222ff1c32ca6cc
Joanne <joannechen84@gmail.com>
2021-07-17 20:49:43 +1000

    added more statuses

--- backend/functions/trivia.js
+++ backend/functions/trivia.js
@@ -1,43 +1,69 @@
-
+/* statuses: 
+    - "" - new account (ask if want to play trivia)
+    - "start" - want to start trivia (ask for options - number of qs)
+    - "difficulty" - want to start trivia (ask for options - difficulty)
+    - "category" - want to start trivia (ask for options - category)
+    - "answering" - in the process of trivia (check answer + get next q - if done, get score of current round)
+    - "" - done with trivia (view total score)
+*/
 async function check_status(db, phone_number, res) {
-    const status = db.collection("trivia")
+    const user = db.collection("trivia")
         .doc(phone_number)
-        .get("status")
+        .get()
+    if (!user.exists) {
+        // create document
+        create_user(db, phone_number)
+        return welcomemsg
+    }
+    const status = user.data["status"]
     if (status == "answering") {
         if (check_trivia_answer(db, phone_number, res)) {
             return "Great Job!\n" + get_trivia_q(db, phone_number)
         } else {
             return "Better Luck next time\n" + get_trivia_q(db, phone_number)
         }
-    }
+    } else if (status == "starting") {
+        // show scoreboard
+        return scoreboard(db, phone_number)
+    } else if (status == "done") {
+        // show scoreboard
+        return scoreboard(db, phone_number)
+    } else if (status == "none") {
+        if (res == "start?") {
+            return start_trivia(db, phone_number)
+        } else {
+            // show scoreboard
+            return scoreboard(db, phone_number)
+        }
+    } 
 }
 
-async function get_trivia_q(db, phone_number) {
-    const next_q = db.collection("trivia")
-    .doc(phone_number)
-    .get("next_question")
-    
-    // set new next q
+function get_category(db, )
 
+async function get_trivia_q(db, phone_number, user) {
+    next_q = user["next_question"]
+    // set new next q
+    if (next_q == "" || next_q == null) {
+        return current_score(db, phone_number, user)
+    }
     return next_q
 }
 
-async function check_trivia_answer(db, phone_number, user_answer) {
-    const answer = db.collection("trivia")
-    .doc(phone_number)
-    .get("last_answer")
+function check_trivia_answer(user, user_answer) {
+    const answer = user["last_answer"]
     if (user_answer.toLowerCase() != answer.toLowerCase()) {
         return True
     }
     return False
 }
 
-async function start_trivia(db, phone_number) {
+async function start_trivia(db, phone_number, questions) {
     const data = {
         next_question: "",
         last_answer: "",
         question_list: [],
-        current_score: 0
+        current_score: 0,
+        num_questions: questions
     }
     const res = await db.collection('trivia').doc(phone_number).set(data);
     const next_q = db.collection("trivia")
@@ -47,6 +73,24 @@ async function start_trivia(db, phone_number) {
         return get_trivia_answer(phone_number)
     }
     return next_q
+}
+
+async function create_user(db, phone_number) {
+    const data = {
+        total_score: 0,
+        status: ""
+    }
+    await db.collection('trivia').doc(phone_number).set(data);
+}
+
+function current_score(user) {
+    const score = user["current_score"]
+    return "You currently have a score of " + str(score) + "\n"
+}
+
+function scoreboard(user) {
+    const score = user["total_score"]
+    return "You currently have a total score of " + str(score) + "\n"
 }
 
 // We should keep a total score of all trivias
