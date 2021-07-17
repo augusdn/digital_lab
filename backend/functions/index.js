f1931abbbe7794dbff7920afd54214285442a04d
augusdn <augusdn@gmail.com>
2021-07-17 20:50:43 +1000

    sms data

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -7,17 +7,35 @@ const app = express();
 // cors settings
 const cors = require('cors');
 app.use(cors({ origin: true }));
+app.use(express.urlencoded())
 
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-app.get('/', (req, res) => {
-    res.contentType("text/xml");
-    res.send(`
-        <Response>
-            <Message> Good Luck! </Message>
-        </Response>
-    `);
+app.post('/', (req, res) => {
+    console.log("------------");
+    console.log(req.body);
+    console.log("------------");
+
+    admin.firestore().collection('sms').add(req.body)
+        .then((result) => {
+            console.log(result.id);
+            res.contentType("text/xml");
+            res.send(`
+            <Response>
+                <Message> sms with ${result.id} added! </Message>
+            </Response>
+        `);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.contentType("text/xml");
+            res.send(`
+            <Response>
+                <Message> ERROR </Message>
+            </Response>
+        `);
+        });
 });
 
 exports.api = functions.region('australia-southeast1').https.onRequest(app);
