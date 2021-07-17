6d549ea77ba939642f17f923dd0c3a45e99c8ecc
irfan <z5176795@student.unsw.edu.au>
2021-07-17 21:27:05 +1000

    can now pass in parameters to fetch customized open trivia data

--- backend/opentrivia/index.js
+++ backend/opentrivia/index.js
@@ -1,7 +1,24 @@
 const fetch = require("node-fetch");
 
-const fetchOpenTrivia = () => {
-    fetch("https://opentdb.com/api.php?amount=10")
+const fetchOpenTrivia = (questions, category, difficulty) => {
+    // the base url (default)
+    var url = "https://opentdb.com/api.php?amount=10";
+    // building url with parameters
+    if (questions > 0 && questions <= 50) {
+        url = "https://opentdb.com/api.php?amount=" + questions;
+    };
+    if (category > 0 && category < 9) {
+        url = url + "&category=" + mapToCategory(category);
+    };
+    if (difficulty === "easy") {
+        url = url + "&difficulty" + difficulty;
+    } else if (difficulty === "medium") {
+        url = url + "&difficulty" + difficulty;
+    } else if (difficulty === "hard") {
+        url = url + "&difficulty" + difficulty;
+    };
+    // fetch and return
+    fetch(url)
     .then((response) => {
         response.json().then((data) => {
             console.log(data)
@@ -10,4 +27,3 @@ const fetchOpenTrivia = () => {
     })
 }
 
-fetchOpenTrivia()
